8512c3566aeafa91d341776df093939fa23726d4
bchou2012 <36647368+bchou2012@users.noreply.github.com>
2018-06-27 15:31:33 -0400

    Added sensor stick scripts folder

---
+++ sensor_stick_scripts/capture_features_project_2.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+import numpy as np
+import pickle
+import rospy
+
+from sensor_stick.pcl_helper import *
+from sensor_stick.training_helper import spawn_model
+from sensor_stick.training_helper import delete_model
+from sensor_stick.training_helper import initial_setup
+from sensor_stick.training_helper import capture_sample
+from sensor_stick.features import compute_color_histograms
+from sensor_stick.features import compute_normal_histograms
+from sensor_stick.srv import GetNormals
+from geometry_msgs.msg import Pose
+from sensor_msgs.msg import PointCloud2
+
+
+def get_normals(cloud):
+    get_normals_prox = rospy.ServiceProxy('/feature_extractor/get_normals', GetNormals)
+    return get_normals_prox(cloud).cluster
+
+
+if __name__ == '__main__':
+    rospy.init_node('capture_node')
+
+    #world2 models
+    models = [\
+       'biscuits',
+       'soap',
+       'book',
+       'soap2',
+       'glue']
+
+    # Disable gravity and delete the ground plane
+    initial_setup()
+    labeled_features = []
+
+    for model_name in models:
+        spawn_model(model_name)
+        iteration = 500 #increased iterations for accuracy
+
+        for i in range(iteration):
+            # make five attempts to get a valid a point cloud then give up
+            sample_was_good = False
+            try_count = 0
+            while not sample_was_good and try_count < iteration:
+                sample_cloud = capture_sample()
+                sample_cloud_arr = ros_to_pcl(sample_cloud).to_array()
+
+                # Check for invalid clouds.
+                if sample_cloud_arr.shape[0] == 0:
+                    print('Invalid cloud detected')
+                    try_count += 1
+                else:
+                    sample_was_good = True
+
+            # Extract histogram features
+            chists = compute_color_histograms(sample_cloud, using_hsv=True)
+            normals = get_normals(sample_cloud)
+            nhists = compute_normal_histograms(normals)
+            feature = np.concatenate((chists, nhists))
+            labeled_features.append([feature, model_name])
+
+        delete_model()
+
+
+    pickle.dump(labeled_features, open('training_set_2.sav', 'wb')) #saved to own training set
+
